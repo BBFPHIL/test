60cb75323d8128f6b71032905cb92fcf1ee2a5d7
Phillip Walker <pwalker@fordham.edu>
2013-03-14 17:36:24 -0400

    members view fix

--- application/views/members_area.php
+++ application/views/members_area.php
@@ -4,13 +4,13 @@
 
 <?php foreach($rows as $r): ?>
 
-<h3><?php echo $r->first_name; ?>
+<h3><?php echo $r->first_name; ?></h3>
 
-<h3><?php echo $r->last_name; ?>
+<h3><?php echo $r->last_name; ?></h3>
 
-<h3><?php echo $r->email_address; ?>
+<h3><?php echo $r->username; ?></h3>
 
-<h3><?php echo $r->username; ?>
+<h3><?php echo $r->email_address; ?></h3>
 
 <?php endforeach ?> 
 
